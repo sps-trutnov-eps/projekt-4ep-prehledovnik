f5bd749d3541b0fae27cc4c2a9adf5660b9ff4c0
Michálek Martin <it.spyglass004@passinbox.com>
2024-09-19 16:16:01 +0200

    backend funguje

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const app = express();
 
-const app = express.app;
+app.use(express.static('www'));
 
-module.exports = app;
+app.get('/', (req, res) => res.sendFile('html/index.html', {root: __dirname }));
+app.get('/test', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
