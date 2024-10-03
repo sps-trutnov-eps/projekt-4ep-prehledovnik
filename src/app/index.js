4a05fc0346ca0f32628bda28882817eee9af62b2
Šimon Kačer <kacers21@spstrutnov.cz>
2024-10-03 15:02:38 +0200

    Může se začít tvořit stránka pro maturity.

--- src/app/index.js
+++ src/app/index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static("./app/views"));
+
+app.get("*", (req,res) => {
+    res.redirect("maturity.html");
+});
+
 module.exports = app;
