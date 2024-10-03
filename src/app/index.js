ed77f2bf5f4b61178cab9a89c81003f089cd196a
Michálek Martin <michalekm21@spstrutnov.cz>
2024-10-03 18:49:54 +0200

    chore: add tempate for each page

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('www'));
+app.use(express.static("www"));
 
-app.get('/', (req, res) => res.sendFile('html/index.html', {root: __dirname }));
-app.get('/test', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) =>
+  res.sendFile("html/index.html", { root: __dirname }),
+);
+app.get("/rozvrh", (req, res) =>
+  res.sendFile("html/rozvrh.html", { root: __dirname }),
+);
+app.get("/osnovy", (req, res) =>
+  res.sendFile("html/osnovy.html", { root: __dirname }),
+);
+app.get("/udalosti", (req, res) =>
+  res.sendFile("html/udalosti.html", { root: __dirname }),
+);
+app.get("/ukoly", (req, res) =>
+  res.sendFile("html/ukoly.html", { root: __dirname }),
+);
+app.get("/maturita", (req, res) =>
+  res.sendFile("html/maturita.html", { root: __dirname }),
+);
 
-app.listen(3000, () => console.log('App listening on port 3000!'));
+app.listen(3000, () => console.log("App listening on port 3000!"));
