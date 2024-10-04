58830efe5b73936b8c0bd97b49ae174daaf3a348
Karel Kříž <KrizK21@spstrutnov.cz>
2024-10-04 16:03:00 +0200

    dodělání stránek a examplu

--- src/app/index.js
+++ src/app/index.js
@@ -3,14 +3,16 @@ const app = express();
 
 require('dotenv').config();
 
-app.use(express.static('www'));
+app.use(express.static('./www'));
 app.use(express.json());
 
 // ejs init
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.urlencoded({ extended: true }));
+
 // app.use, NIKOLI app.get!
 app.use('/', require("./routers/defaultRouter.js"));
 
-app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`));
+app.listen(8000, () => console.log(`App listening on port ${8000}!`));
---
+++ src/www/example/js/main.js
@@ -0,0 +1,29 @@
+function writeIntoServerConsole(txt) {
+
+	let anotherOne = document.getElementById('anotherOne').value;
+	
+	// Tohle vypíše do konzole prohlížeče
+	console.log('sending text to server console.');
+	
+	// To co je tu v závorkách je url adresa
+	// je relativní takže stačí napsat např: '/kalendar/ulozit'
+	// (pokračování v exampleRouter.js)
+	fetch('/example/writeOutIntoConsole', {
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json",
+		  },
+		// Toto je obsah toho co posíláme serveru
+		// v tomto případě posíláme .json s obsahem...
+		body: JSON.stringify({ "textToWriteOut": txt, "anotherOne": anotherOne})
+	})
+	.then(response => response.text())
+	.then(data => {
+		// Tohle vypíše do konzole prohlížeče odpověď ze serveru
+		console.log('Data from server: ' + data);
+	})
+	.catch(error => {
+		// Pokud někde vznikne chyba tak se to vypíše do konzole
+		console.error('Error:', error);
+	});
+}
