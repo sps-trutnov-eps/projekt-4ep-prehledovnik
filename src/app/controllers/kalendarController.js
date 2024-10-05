027d64730e4f703fe72d90a2bb5e52cb328e9d62
Marek Langer <langerm21@spstrutnov.cz>
2024-10-05 19:30:55 +0200

    Počátek měsíčního kalendáře

--- src/app/controllers/kalendarController.js
+++ src/app/controllers/kalendarController.js
@@ -1,1 +1,10 @@
 const databaze = require("../models/databaseEngine");
+
+const d = new Date();
+
+exports.month = () => {
+    return d.getMonth();
+};
+exports.day = () =>{
+   return d.getDate();
+};
--- src/app/routers/kalendarRouter.js
+++ src/app/routers/kalendarRouter.js
@@ -1,6 +1,7 @@
 const kalendarRouter = require('express').Router();
+const kalendarController = require("../controllers/kalendarController");
 
-kalendarRouter.get('/', (req, res) => res.render('kalendar/index.ejs', {}));
+kalendarRouter.get('/', (req, res) => res.render('kalendar/index.ejs', {day: kalendarController.day(), month: kalendarController.month()}));
 kalendarRouter.get('/tydenni', (req, res) => res.render('kalendar/tydenni.ejs', {}));
 
 module.exports = kalendarRouter;
