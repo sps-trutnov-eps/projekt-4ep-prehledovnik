f7421534a503ba4abaa26f4c2ac3a43f416d9b47
Marek Langer <langerm21@spstrutnov.cz>
2024-10-05 17:48:30 +0200

    Základní struktura

--- src/app/routers/kalendarRouter.js
+++ src/app/routers/kalendarRouter.js
@@ -1,5 +1,6 @@
 const kalendarRouter = require('express').Router();
 
 kalendarRouter.get('/', (req, res) => res.render('kalendar/index.ejs', {}));
+kalendarRouter.get('/tydenni', (req, res) => res.render('kalendar/tydenni.ejs', {}));
 
 module.exports = kalendarRouter;
