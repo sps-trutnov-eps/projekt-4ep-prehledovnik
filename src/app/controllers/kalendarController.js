9fa6d1f37827e6b5be8fee2377646424220fca66
Marek Langer <langerm21@spstrutnov.cz>
2024-10-08 20:30:02 +0200

    Podpora rozvrhu

--- src/app/controllers/kalendarController.js
+++ src/app/controllers/kalendarController.js
@@ -1,6 +1,10 @@
 const databaze = require("../models/databaseEngine");
 const udalost = [27, 9, "Událost"];
+const rozvrh = ["", "PVA", "", "CJ", "MAT", "", "", "", "", "Krouzek"]
 
 exports.date_udalost = () => {
     return udalost;
+};
+exports.rozvrh = () => {
+    return rozvrh;
 };
--- src/app/routers/kalendarRouter.js
+++ src/app/routers/kalendarRouter.js
@@ -2,6 +2,6 @@ const kalendarRouter = require('express').Router();
 const kalendarController = require("../controllers/kalendarController");
 
 kalendarRouter.get('/', (req, res) => res.render('kalendar/index.ejs', {date_udalost: kalendarController.date_udalost()}));
-kalendarRouter.get('/tydenni', (req, res) => res.render('kalendar/tydenni.ejs', {}));
+kalendarRouter.get('/tydenni', (req, res) => res.render('kalendar/tydenni.ejs', {rozvrh: kalendarController.rozvrh()}));
 
 module.exports = kalendarRouter;
