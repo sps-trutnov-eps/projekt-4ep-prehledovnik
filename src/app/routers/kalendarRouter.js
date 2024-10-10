7bd4983ba926b86252ff3bbb2a1fa07f9eb66d5c
Marek Langer <langerm21@spstrutnov.cz>
2024-10-10 09:36:25 +0200

    Můžeme získat číslo prvního týdne

--- src/app/routers/kalendarRouter.js
+++ src/app/routers/kalendarRouter.js
@@ -2,6 +2,6 @@ const kalendarRouter = require('express').Router();
 const kalendarController = require("../controllers/kalendarController");
 
 kalendarRouter.get('/', (req, res) => res.render('kalendar/index.ejs', {date_udalost: kalendarController.date_udalost()}));
-kalendarRouter.get('/tydenni', (req, res) => res.render('kalendar/tydenni.ejs', {rozvrh: kalendarController.rozvrh()}));
+kalendarRouter.get('/tydenni', (req, res) => res.render('kalendar/tydenni.ejs', {rozvrh: kalendarController.rozvrh(), week: kalendarController.week()}));
 
 module.exports = kalendarRouter;
