60d5cd94533b00b759ffb659c7ca8b89a9c82f7f
Pavel Kotek <kotekp21@spstrutnov.cz>
2024-10-10 15:28:29 +0200

    feat(bcknd): rest of functions for "projekty" part

--- src/app/models/databaseEngine.js
+++ src/app/models/databaseEngine.js
@@ -200,6 +200,52 @@ const projekty = {
         projekty[projekty["nextID"]] = {trida, tymy, pitche, milestony, devlogy, prezentace};
         projekty["nextID"] += 1;
         sP(projekty);
+    },
+    upravitTymy: (trida, tymy) => {
+        let projekty = gP();
+        let IDUpravovanehoProjektu = hledanyProjekt(trida);
+        projekty[IDUpravovanehoProjektu]["tymy"] = tymy;
+        sP(projekty);
+    },
+    upravitPitche: (trida, pitche) => {
+        let projekty = gP();
+        let IDUpravovanehoProjektu = hledanyProjekt(trida);
+        projekty[IDUpravovanehoProjektu]["ptiche"] = pitche;
+        sP(projekty);
+    },
+    upravitMilestony: (trida, milestony) => {
+        let projekty = gP();
+        let IDUpravovanehoProjektu = hledanyProjekt(trida);
+        projekty[IDUpravovanehoProjektu]["milestony"] = milestony;
+        sP(projekty);
+    },
+    upravitDevlogy: (trida, devlogy) => {
+        let projekty = gP();
+        let IDUpravovanehoProjektu = hledanyProjekt(trida);
+        projekty[IDUpravovanehoProjektu]["devlogy"] = devlogy;
+        sP(projekty);
+    },
+    upravitPrezentace: (trida, prezentace) => {
+        let projekty = gP();
+        let IDUpravovanehoProjektu = hledanyProjekt(trida);
+        projekty[IDUpravovanehoProjektu]["prezentace"] = prezentace;
+        sP(projekty);
+    },
+    ziskatIDProjektu: (trida) => {
+        let projekty = gP();
+        let nextID = projekty["nextID"];
+        let IDHledanehoProjektu;
+        for(let i = 0; i < nextID; i++){
+            if(projekty[String(i)]["trida"] == trida){
+                IDHledanehoProjektu = String(i);
+            }
+        }
+        return IDHledanehoProjektu;
+    },
+    ziskatProjekt: (trida) => {
+        let projekty = gP();
+        let ID = ziskatIDProjektu(trida);
+        return projekty[ID];
     }
 }
 
