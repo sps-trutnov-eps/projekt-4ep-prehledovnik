b105a30f288cef8ff6ff0b058b56b4e36e61839a
Matěj Kroulík <kroulikm21@spstrutnov.cz>
2024-10-11 16:40:27 +0200

    zaklad Controlleru, routeru projektu

--- src/app/controllers/projektyController.js
+++ src/app/controllers/projektyController.js
@@ -1,1 +1,17 @@
 const databaze = require("../models/databaseEngine");
+
+exports.zobrazTymy = (req, res) => {
+    res.render("projekty/tymy.ejs")
+}
+
+exports.zobrazPitche = (req, res) => {
+    res.render("projekty/tymy/pitch.ejs")
+}
+
+exports.zobrazPrezentace = (req, res) => {
+    res.render("projekty/tymy/prezentace.ejs")
+}
+
+exports.zobrazDetailyTymu = (req, res) => {
+    res.render("projekty/tymy/detailTymu.ejs")
+}
--- src/app/routers/projektyRouter.js
+++ src/app/routers/projektyRouter.js
@@ -2,5 +2,8 @@ const projektyRouter = require('express').Router();
 const projektyController = require('../controllers/projektyController');
 
 projektyRouter.get('/', (req, res) => res.render('projekty/index.ejs', {}));
-
+projektyRouter.get('/tymy', projektyController.zobrazTymy());
+projektyRouter.get('/tymy/tym', projektyController.zobrazDetailyTymu());
+projektyRouter.get('/tymy/pitche', projektyController.zobrazPitche());
+projektyRouter.get('/tymy/prezentace', projektyController.zobrazPrezentace());
 module.exports = projektyRouter;
