4fbe4e55acec4977bdca1565c74af470d326c78f
Tomáš Svoboda <svobodat21@spstrutnov.cz>
2024-10-13 17:33:34 +0200

    presmerovani stranek

--- src/app/controllers/maturityController.js
+++ src/app/controllers/maturityController.js
@@ -1,1 +1,14 @@
 const databaze = require("../models/databaseEngine");
+
+exports.pcmz = (req, res) => {
+    res.render("maturity/pcmz.ejs");
+}
+
+exports.sloh = (req, res) => {
+    res.render("maturity/sloh.ejs");
+}
+
+exports.pzop = (req, res) => {
+    res.render("maturity/pzop.ejs");
+}
+
