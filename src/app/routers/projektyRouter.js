1a1fb1e065e99f93a6251e0779fb43884719ee64
Matěj Kroulík <kroulikm21@spstrutnov.cz>
2024-10-13 14:44:52 +0200

    oprava routeru

--- src/app/routers/projektyRouter.js
+++ src/app/routers/projektyRouter.js
@@ -2,8 +2,8 @@ const projektyRouter = require('express').Router();
 const projektyController = require('../controllers/projektyController');
 
 projektyRouter.get('/', (req, res) => res.render('projekty/index.ejs', {}));
-projektyRouter.get('/tymy', projektyController.zobrazTymy());
-projektyRouter.get('/tymy/tym', projektyController.zobrazDetailyTymu());
-projektyRouter.get('/tymy/pitche', projektyController.zobrazPitche());
-projektyRouter.get('/tymy/prezentace', projektyController.zobrazPrezentace());
+projektyRouter.get('/tymy', (req, res) => res.render('projekty/tymy.ejs', {tymy: projektyController.zobrazTymy()}));
+projektyRouter.get('/tymy/tym', (req, res) => res.render('projekty/detailTymu.ejs', {detailTymu: projektyController.zobrazDetailyTymu()}));
+projektyRouter.get('/tymy/pitche', (req, res) => res.render('projekty/pitch.ejs', {pitche: projektyController.zobrazPitche()}));
+projektyRouter.get('/tymy/prezentace', (req, res) => res.render('projekty/prezentace.ejs', {prezentace: projektyController.zobrazPrezentace()}));
 module.exports = projektyRouter;
