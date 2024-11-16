3a9cab90d4cefa44d49e15a98167d49b46d61da2
Kačer Šimon <kacers21@spstrutnov.cz>
2024-11-16 23:10:19 +0100

    úprava maturityController

--- src/app/controllers/maturityController.js
+++ src/app/controllers/maturityController.js
@@ -14,28 +14,61 @@ exports.scmz = (req, res) => {
 }
 
 exports.ukladanipzop = (req, res) => {
-    let den_konani = req.body.den_konani
-    let dodatecne_dny = req.body.dodatecne_dny
-    let ucebna = req.body.ucebna
+    const {den_konani, dodatecne_dny, ucebna} = req.body;
+    console.log("Přijatá data:", {den_konani, dodatecne_dny, ucebna});
 
- 
+    if (!den_konani) {
+        res.redirect("/maturity/");
+    }
+    else if (!den_konani && dodatecne_dny) {
+        res.redirect("/maturity/");
+    }
+    else {
+        databaze.maturity.pridatMaturitniEvent("PŽOP",[den_konani, dodatecne_dny].filter((den) => den), [], ucebna);
+    }
 
-    res.redirect("/maturity/")
+    res.redirect("/maturity/");
 }
 
 exports.ukladanipcmz = (req, res) => {
-    let den1_datum = req.body.den1_datum;
-    let zaskrtnuteHodiny = [];
+    const body = req.body;
+    let vsechnyDnyUlozeny = false;
 
-    for (let i = 1; i <= 9; i++) {
-        if (req.body[`den1_hodina${i}`]) {
-            zaskrtnuteHodiny.push(`hodina${i}`);
+    const radky = [];
+    let pocitadloDnu = 1;
+
+    while (!vsechnyDnyUlozeny) {
+        const dateKey = `den${pocitadloDnu}_datum`;
+
+        if (!body[dateKey]) {
+            vsechnyDnyUlozeny = true;
         }
+
+        const hodiny = [];
+        for (let i = 1; i <= 9; i++) {
+            const hourKey = `den${pocitadloDnu}_hodina${i}`;
+            if (body[hourKey]) {
+                hodiny.push(i); 
+            }
+        }
+
+        if (body[dateKey] && hodiny.length > 0) {
+            radky.push({
+                datum: body[dateKey],
+                hodiny: hodiny,
+            });
+        }
+
+        pocitadloDnu++;
     }
 
- 
-    res.redirect("/maturity/pcmz")
+    radky.forEach((radek) => {
+        databaze.maturity.pridatMaturitniEvent("PČMZ", [radek.datum], radek.hodiny, null); 
+    });
+
+    res.redirect("/maturity/pcmz");
 };
+
 
 
 exports.ukladaniscmz = (req, res) => {
