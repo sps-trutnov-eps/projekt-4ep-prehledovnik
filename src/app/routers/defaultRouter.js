edcd05ad0b6cdf4e0181a0a993ac61b940570813
Jakub Polák <polakj21@spstrutnov.cz>
2025-02-13 09:25:50 +0100

    dokumentace na úvodní straně

--- src/app/routers/defaultRouter.js
+++ src/app/routers/defaultRouter.js
@@ -1,6 +1,15 @@
 const defaultRouter = require('express').Router();
+const markdown = require( "markdown" ).markdown;
+const fs = require('node:fs');
 
-defaultRouter.get('/', (req, res) => res.render('index.ejs', {}));
+defaultRouter.get('/', (req, res) => {
+    fs.readFile('../doc/pouziti.md', 'utf8', (err, data) => {
+        if (err) console.error(err);
+
+        res.render('index.ejs', {doc: markdown.toHTML(data)});
+    });
+});
+
 defaultRouter.get('/test', (req, res) => res.send('Hello World!'));
 
 defaultRouter.use('/kalendar', require('./kalendarRouter.js'));
