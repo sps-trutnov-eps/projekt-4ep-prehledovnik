b96cce90a11280f44675f486a745e2a64059d380
Jakub Polák <polakj21@spstrutnov.cz>
2025-03-02 10:29:08 +0100

    konzistentní barvy se zbytkem aplikace

--- src/app/routers/defaultRouter.js
+++ src/app/routers/defaultRouter.js
@@ -15,7 +15,7 @@ defaultRouter.get('/', (req, res) => {
         content = htmlContent.replace(/<h(\d)>(.*?)<\/h\1>/g, (match, level, text) => {
             sectionId++;
             const id = `section-${sectionId}`;
-            navLinks += `<li><a href="#${id}">${text}</a></li>`;
+            navLinks += `<li><a href="#${id}"><button>${text}</button></a></li>`;
             return `<h${level} id="${id}">${text}</h${level}>`;
         });
 
